78b38667125160fbcf7d83498188661fb65a086f
piotrmatuszewski <matuszewski.piotrw@gmail.com>
2022-11-23 23:46:54 +0100

    Updated personal identifier validation

--- src/main/kotlin/pl/pm/currencyaccounts/account/validation/AdultAgeValidation.kt
+++ src/main/kotlin/pl/pm/currencyaccounts/account/validation/AdultAgeValidation.kt
@@ -1,6 +1,8 @@
 package pl.pm.currencyaccounts.account.validation
 
 import org.springframework.stereotype.Component
+import pl.foltak.polishidnumbers.pesel.Pesel
+import pl.foltak.polishidnumbers.pesel.PeselValidator
 import pl.pm.currencyaccounts.account.model.dto.AccountDTO
 import pl.pm.currencyaccounts.core.util.isAdult
 import java.time.LocalDate
@@ -9,20 +11,8 @@ import java.time.LocalDate
 class AdultAgeValidation : AccountValidation {
 
     override fun invoke(account: AccountDTO): Boolean =
-        LocalDate.of(
-            getBirthYear(account.personalId),
-            getBirthMonth(account.personalId),
-            getBirthDay(account.personalId)
-        )
-            .let(LocalDate::isAdult)
-
-    private fun getBirthYear(str: String) =
-        str.substring(0, 2).toInt()
-
-    private fun getBirthMonth(str: String) =
-        str.substring(2, 4).toInt()
-
-    private fun getBirthDay(str: String) =
-        str.substring(4, 6).toInt()
-
+        Pesel(account.personalId)
+            .birthDate
+            ?.let(LocalDate::isAdult)
+            ?:false
 }
--- src/main/kotlin/pl/pm/currencyaccounts/account/validation/PersonalNumberValidation.kt
+++ src/main/kotlin/pl/pm/currencyaccounts/account/validation/PersonalNumberValidation.kt
@@ -1,15 +1,12 @@
 package pl.pm.currencyaccounts.account.validation
 
 import org.springframework.stereotype.Component
+import pl.foltak.polishidnumbers.pesel.PeselValidator
 import pl.pm.currencyaccounts.account.model.dto.AccountDTO
 
 @Component
 class PersonalNumberValidation : AccountValidation {
 
-    companion object {
-        private const val PERSONAL_ID_MAX_LENGTH = 11;
-    }
-
     override fun invoke(account: AccountDTO): Boolean =
-        account.personalId.length <= PERSONAL_ID_MAX_LENGTH
+        PeselValidator().isValid(account.personalId)
 }
