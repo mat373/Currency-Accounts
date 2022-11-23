319b5b9a0925c124ebf91f922018269a64346f94
piotrmatuszewski <matuszewski.piotrw@gmail.com>
2022-11-23 23:25:22 +0100

    Update application configuration

--- src/main/kotlin/pl/pm/currencyaccounts/core/config/ApplicationConfig.kt
+++ src/main/kotlin/pl/pm/currencyaccounts/core/config/ApplicationConfig.kt
@@ -1,10 +1,8 @@
 package pl.pm.currencyaccounts.core.config
 
-
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories
 import org.springframework.scheduling.annotation.EnableScheduling
-
 @Configuration
 @EnableScheduling
 @EnableR2dbcRepositories
